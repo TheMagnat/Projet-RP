74d5bb7c07705b06fbdf93e5587568d9ecf574e0
Guillaume Magniadas <guillaume.magniadas@gmail.com>
2021-05-28 05:32:59 +0200

    Cleaned last

--- src/helper.py
+++ src/helper.py
@@ -136,6 +136,64 @@ def sortedDurationWithArrive(durations, arrive):
     indexes = np.argwhere(arrive == 0).ravel()
 
     iteration = 0
+
+    #Note: isclose because of python precision
+    while not (np.abs(timeSpent - durations) < 1.e-8).all():
+
+        iteration += 1
+
+        notFinished = ~(np.abs(timeSpent[indexes] - durations[indexes]) < 1.e-8)
+
+        miniValue = np.inf
+
+        if notFinished.sum() > 0:
+
+            mini = (durations[indexes][notFinished] - timeSpent[indexes][notFinished]).argmin()
+
+            miniIndex = indexes[notFinished][mini]
+
+            miniValue = durations[miniIndex] - timeSpent[miniIndex]
+
+
+        mask = np.ones(arrive.shape, np.bool_)
+        mask[indexes] = False
+
+        mask = np.argwhere(mask).ravel()
+
+        timeToSpent = min((arrive[mask] - totalTime).min(), miniValue) if mask.size > 0 else miniValue
+
+
+        totalTime += timeToSpent
+
+        #Add only if there is a task
+        if notFinished.sum() > 0:
+            timeSpent[miniIndex] += timeToSpent
+
+        done = np.argwhere(np.abs(timeSpent - durations) < 1.e-8).ravel()
+
+        score += (states[done] != 1).sum() * totalTime
+
+        states[done] = 1
+
+
+        close = np.argwhere(np.abs(arrive[mask] - totalTime) < 1.e-8).ravel()
+
+        indexes = np.concatenate( (indexes, mask[close]) )
+        indexes.sort()
+
+    return score
+
+"""
+def sortedDurationWithArrive(durations, arrive):
+    timeSpent = np.zeros(durations.size, dtype=np.float64)
+    states = np.zeros(durations.size, dtype=np.int8)
+
+    totalTime = 0
+    score = 0
+
+    indexes = np.argwhere(arrive == 0).ravel()
+
+    iteration = 0
     
     scoreArr = []
     totalTimeArr = []
@@ -221,6 +279,8 @@ def sortedDurationWithArrive(durations, arrive):
     return score, totalTimeArr, scoreArr, tacheTimeArr
 
 
+"""
+
 
 
 """
@@ -234,6 +294,86 @@ It sum should be equal to 1.
 
 """
 #@njit(cache=True)
+def executeAlgorithm(durations, predictions, algorithm, arrive=np.empty(0)):
+
+    if arrive.size == 0:
+        arrive = np.zeros(durations.size)
+
+    currentIndexes = np.argwhere(arrive == 0).ravel()
+
+    n = len(durations)
+    states = np.zeros(n, dtype=np.int8)
+    timeSpent = np.zeros(n, dtype=np.float64)
+
+    iteration = 0
+    score = 0
+
+    totalTime = 0
+
+
+    while states.sum() < n:
+
+        iteration += 1
+
+        smallestCoef = np.inf
+
+        #This test make sure there is a task available
+        if np.argwhere(states[currentIndexes] < 1).ravel().size > 0:
+
+            allocation = algorithm( len(currentIndexes), predictions[currentIndexes], states[currentIndexes] )
+
+            # if not math.isclose(allocation.sum(), 1):
+            #     print("Allocation sum not equal to 1, cheating")
+            #     return -1
+
+            #smallestCoef = np.inf
+            for alloc, (alreadySpent, dura) in zip(allocation, zip(timeSpent[currentIndexes], durations[currentIndexes])):
+
+                if alloc:
+
+                    rez = (dura - alreadySpent) / alloc
+
+                    if rez < smallestCoef:
+                        smallestCoef = rez
+
+        else:
+            allocation = np.zeros(currentIndexes.size)
+
+
+        mask = np.ones(arrive.shape, np.bool_)
+        mask[currentIndexes] = False
+
+        mask = np.argwhere(mask).ravel()
+
+
+        #Take the min time between the next finished task with the algorithm allocation or the next new task
+        smallestCoef = min((arrive[mask] - totalTime).min(), smallestCoef) if mask.size > 0 else smallestCoef
+
+
+
+        totalTime += smallestCoef
+
+        timeSpent[currentIndexes] += allocation * smallestCoef
+
+        #Note: we're using here < 1.e-8 instead of using numpy.isclose because of the use of numba.
+        done = np.argwhere(np.abs(timeSpent - durations) < 1.e-8).ravel()
+
+        score += (states[done] != 1).sum() * totalTime
+
+
+        states[done] = 1
+
+        #Verify if we add task
+        #np.abs(arrive[mask] - totalTime) < 1.e-8
+        close = np.argwhere(np.abs(arrive[mask] - totalTime) < 1.e-8).ravel()
+
+        currentIndexes = np.concatenate( (currentIndexes, mask[close]) )
+        currentIndexes.sort()
+
+    return score
+
+#To return more data
+"""
 def executeAlgorithm(durations, predictions, algorithm, arrive=np.empty(0)):
 
     if arrive.size == 0:
@@ -330,91 +470,14 @@ def executeAlgorithm(durations, predictions, algorithm, arrive=np.empty(0)):
 
     return score, totalTimeArr, scoreArr, tacheTimeArr
 
-#Olf version without arrive
+
 """
-def executeAlgorithm(durations, predictions, algorithm):
-
-    n = len(durations)
-    states = np.zeros(n, dtype=np.int)
-    timeSpent = np.zeros(n, dtype=np.float64)
-
-
-    iteration = 0
-    score = 0
-
-    totalTime = 0
-
-    while states.sum() < n:
-
-        iteration += 1
-
-        allocation = algorithm(n, predictions, states.copy())
-
-        if not np.isclose(allocation.sum(), 1):
-            print("Allocation sum not equal to 1, cheating")
-            return -1
-
-        smallestCoef = np.inf
-        for alloc, (alreadySpent, dura) in zip(allocation, zip(timeSpent, durations)):
-
-            if alloc:
-
-                rez = np.linalg.solve(a=[[ alloc ]], b=[dura - alreadySpent])
-
-                if rez[0] < smallestCoef:
-                    smallestCoef = rez[0]
-
-
-        totalTime += smallestCoef
-
-        timeSpent += allocation * smallestCoef
-
-        done = np.argwhere(np.isclose(timeSpent, durations)).ravel()
-
-        score += (states[done] != 1).sum() * totalTime
-
-        states[done] = 1
-
-
-    return score
-"""
-
-
-#One by one version, not working with predRoundRobin
-# def executeAlgorithm(durations, predictions, algorithm):
-
-#     n = len(durations)
-#     states = np.zeros(n, dtype=np.int)
-#     timeSpent = np.zeros(n, dtype=np.float64)
-
-
-#     iteration = 0
-#     score = 0
-#     while states.sum() < n:
-
-#         iteration += 1
-
-#         allocation = algorithm(n, predictions, states.copy())
-
-#         if allocation.sum() > 1:
-#             print("Allocation > 1, cheating")
-
-#         timeSpent += allocation
-
-#         done = np.argwhere(np.isclose(timeSpent, durations)).ravel()
-
-#         score += (states[done] != 1).sum() * iteration
-
-#         states[done] = 1
-
-#     return score
-
 
 
 def rapport(optimum, solution):
     return solution/optimum
 
-
+"""
     
 #Debug
 import algorithm
@@ -470,3 +533,4 @@ if __name__ == "__main__":
     plt.show()
 
 
+"""
